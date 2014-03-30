629458aed474959c2f2158cd05bef1bb9daf5ac3
Paolo Antinori <pantinor@redhat.com>
2014-03-30 15:43:09 +0100

    Added oracle jdk 8

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NAMES='openjdk7 openjdk7_maven oraclejdk7 oraclejdk7_maven oraclejdk6 oraclejdk6_maven'
+NAMES='openjdk7 openjdk7_maven oraclejdk7 oraclejdk7_maven oraclejdk6 oraclejdk6_maven oraclejdk8 oraclejdk8_maven'
 
 for name in $NAMES; do
 	echo "** Building $name."
