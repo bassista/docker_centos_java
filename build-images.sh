d03596d162338783ef8ba6f00a60034a921ce71f
Paolo Antinori <pantinor@redhat.com>
2014-04-03 11:59:55 +0100

    Refactored and added helper scripts specific for hibernate build jobs. Added explicit JAVA_HOME and LANG values.

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-NAMES='openjdk7 openjdk7_maven oraclejdk7 oraclejdk7_maven oraclejdk6 oraclejdk6_maven oraclejdk8 oraclejdk8_maven'
+##### openjdk7
+(cd openjdk/7/base ; docker build -t openjdk7:base .)
+(cd openjdk/7/maven ; docker build -t openjdk7:maven .)
+(cd openjdk/7/hibernate/base ; docker build -t openjdk7:hibernate_base .)
+(cd openjdk/7/hibernate/maven ; docker build -t openjdk7:hibernate_maven .)
+(cd openjdk/7/hibernate/gradle ; docker build -t openjdk7:hibernate_gradle .)
 
-for name in $NAMES; do
-	echo "** Building $name."
-    docker build -t $name - < Dockerfile."$name"
-    echo ""
-done
+##### oraclejdk
+(cd oraclejdk/6/base ; docker build -t oraclejdk6:base .)
+(cd oraclejdk/6/maven ; docker build -t oraclejdk6:maven .)
+(cd oraclejdk/7/base ; docker build -t oraclejdk7:base .)
+(cd oraclejdk/7/maven ; docker build -t oraclejdk7:maven .)
+(cd oraclejdk/8/base ; docker build -t oraclejdk8:base .)
+(cd oraclejdk/8/maven ; docker build -t oraclejdk8:maven .)
+
+docker images
---
+++ openjdk/7/hibernate/maven/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+IP_CONTAINER=$(ip -f inet -o addr show eth0 | cut -d ' '  -f 7 | cut -d '/' -f 1)
+service iptables start
+
+### specify your desired iptables here
+
+# add rule to accept connection to docker rest api
+iptables -A INPUT -p tcp --dport 4243 -j ACCEPT;
+# allow all connections already instantiated
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -p tcp --destination $IP_CONTAINER -j ACCEPT;
+
+# accept connections to mongo
+iptables -A INPUT -p tcp --dport 27017 -j ACCEPT;
+
+# accept connections to couch
+iptables -A INPUT -p tcp --dport 5984 -j ACCEPT;
+
+#iptables -A OUTPUT -p tcp  -j REJECT;
+
+# exec is used to correctly handle ctrl signals like CTRL+C
+exec mvn "$@"
